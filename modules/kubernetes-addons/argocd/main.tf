26232d354698a54b4729d6f8b368c252d51c07fd
kahirokunn <okinakahiro@gmail.com>
2022-08-11 12:23:21 +0900

    test

--- modules/kubernetes-addons/argocd/main.tf
+++ modules/kubernetes-addons/argocd/main.tf
@@ -60,16 +60,7 @@ resource "helm_release" "argocd_application" {
 
   set {
     name = "source.helm.values"
-    value = each.value.add_on_application ? yamlencode(merge(
-      { repo_url = each.value.repo_url },
-      each.value.values,
-      local.global_application_values,
-      local.addon_config
-      )) : yamlencode(merge(
-      { repo_url = each.value.repo_url },
-      each.value.values,
-      local.global_application_values
-    ))
+    value = module.helm_values[each.key].merged
   }
 
   # Destination Config.
